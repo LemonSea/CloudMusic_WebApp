f48b88a7c9585b4c375a53fc33da449988f5755b
LemonSea <2533566560@qq.com>
2019-09-14 01:34:21 +0800

    Recommend的redux完成

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Slider from '../../components/slider';
 import List from '../../components/list';
 import Scroll from '../../baseUI/scroll';
 import styled from 'styled-components';
+
+import { connect } from "react-redux";
+import * as actionCreaters from './store/actionCreators';
 
 export const Content = styled.div`
   position: fixed;
@@ -13,28 +16,56 @@ export const Content = styled.div`
 `
 function Recommend(props) {
 
-    const bannerList = [1, 2, 3, 4].map(item => {
-        return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
-    })
-    const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => {
-        return {
-            id: index,
-            picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
-            playCount: 17171122,
-            name: "[洗澡时听的歌] 浴室里听歌吹泡泡o○o○o○"
-        }
-    });
+    // const bannerList = [1, 2, 3, 4].map(item => {
+    //     return { imageUrl: "http://p1.music.126.net/ZYLJ2oZn74yUz5x8NBGkVA==/109951164331219056.jpg" }
+    // })
+    // const recommendList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item, index) => {
+    //     return {
+    //         id: index,
+    //         picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+    //         playCount: 17171122,
+    //         name: "[洗澡时听的歌] 浴室里听歌吹泡泡o○o○o○"
+    //     }
+    // });
+
+    const { bannerList, recommendList } = props;
+    const bannerListJS = bannerList ? bannerList.toJS() : [];
+    const recommendListJS = recommendList ? recommendList.toJS() : [];
+
+    const { getBannerDataDispatch, getRecommendListDataDispatch } = props;
+
+    useEffect(() => {
+        getBannerDataDispatch();
+        getRecommendListDataDispatch();
+    }, []);
 
     return (
         <Content>
             <Scroll className="list">
                 <div>
-                    <Slider bannerList={bannerList}></Slider>
-                    <List recommendList={recommendList}></List>
+                    <Slider bannerList={bannerListJS}></Slider>
+                    <List recommendList={recommendListJS}></List>
                 </div>
             </Scroll>
         </Content>
     )
 }
-
-export default React.memo(Recommend);
+// 映射Redux全局的state到组件的props上
+const mapStateToProps = (state) => ({
+    // 不要再这里将数据toJS,不然每次diff比对props的时候都是不一样的引用，还是导致不必要的重渲染, 属于滥用immutable
+    bannerList: state.getIn(['recommend', 'bannerList']),
+    recommendList: state.getIn(['recommend', 'recommendList']),
+})
+// 映射dispatch到props上
+const mapDispatchToProps = (dispatch) => {
+    return {
+        getBannerDataDispatch() {
+            dispatch(actionCreaters.getBannerList());
+        },
+        getRecommendListDataDispatch() {
+            dispatch(actionCreaters.getRecommendList());
+        },
+    }
+}
+// 将ui组件包装成容器组件
+export default connect(mapStateToProps, mapDispatchToProps)(React.memo(Recommend))
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,9 @@
+// 合并 reducer 函数
 import { combineReducers } from 'redux-immutable';
+// 导入分仓库的 reducer
+import { reducer as recommendReducer } from '../application/Recommend/store/index';
 
+// 合并 reducer 函数为一个 obj
 export default combineReducers({
-    
+    recommend: recommendReducer,
 })
